c749f73a6be08f767bed485c68ef4873c117daa2
Moumitaprofile11 <moumitamondal.career@gmail.com>
2023-03-25 14:09:57 +0530

    commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'to-do';
+  quote = "Dashboard"; 
+  today:Date | undefined;
+  tasks = [
+    "task 1",
+    "task 2",
+    "task 3",
+    "task 4",
+    "task 5",
+    "task 6",
+    
+    
+    
+  ]
+  
+
+
+  constructor() {
+    this.today = new Date();
+  }
+  editTask(){
+
+  }
+  deleteTask(){
+
+  }
 }
